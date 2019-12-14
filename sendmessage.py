e47992cbb681886e958d71e753c51de5c72f4270
High-Bee <ko.yb112@gmail.com>
2019-12-14 18:02:45 +0900

    chatbot practice

--- sendmessage.py
+++ sendmessage.py
@@ -2,7 +2,7 @@ import requests
 from decouple import config
 
 url = 'https://api.telegram.org'
- token = config('TELEGRAM_BOT_TOKEN')
+token = config('TELEGRAM_BOT_TOKEN')
 # chat_id = requests.get(f"{url}/bot{token}/getUpdates").json()["result"][0]["message"]["from"]["id"]
 chat_id = config('CHAT_ID')
 text = input('메세지를 입력하세요: ')
